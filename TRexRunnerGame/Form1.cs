7dac28ef336dd7dd6d66037b3c1e0611fe47ebea
Taylor Masiello <taylor.masiello@outlook.com>
2024-06-22 17:34:02 -0700

    Coded basic game mechanics and scoring

--- TRexRunnerGame/Form1.cs
+++ TRexRunnerGame/Form1.cs
@@ -12,9 +12,21 @@ namespace TRexRunnerGame
 {
     public partial class Form1 : Form
     {
+        bool jumping = false;
+        int jumpSpeed;
+        int force = 12; //foce is limit of how far player can jump up
+        int score = 0;
+        int obstacleSpeed = 10;
+        Random rand = new Random();
+        int position;
+        bool isGameOver = false;
+
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); //initalizes all components of winForm; instance of constructor
+
+            GameReset();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -24,16 +36,92 @@ namespace TRexRunnerGame
 
         private void MainGameTimerEvent(object sender, EventArgs e)
         {
+            TRex.Top += jumpSpeed;
 
+            txtScore.Text = "Score: " + score;
+
+            if (jumping == true && force < 0)
+            {
+                jumping = false;
+            }
+
+            if (jumping == true)
+            {
+                jumpSpeed = -12;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 12;
+            }
+
+            if (TRex.Top > 300 && jumping == false) //will force tRex down once force reaches lower limit
+            {
+                force = 12;
+                TRex.Top = 301;
+                jumpSpeed = 0;
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+
+                    if (x.Left < -100)
+                    {
+                        x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15);
+                        score++;
+                    }
+                }
+            }
         }
 
         private void keyIsDown(object sender, KeyEventArgs e)
         {
-
+            if (e.KeyCode == Keys.Space && jumping == false) //only want to jump when not already jumping
+            {
+                jumping = true;
+            }
         }
 
         private void keyIsUp(object sender, KeyEventArgs e)
         {
+            if (jumping == true)
+            {
+                jumping = false;
+            }
+
+            if (e.KeyCode == Keys.R && isGameOver == true)
+            {
+                GameReset();
+            }
+        }
+
+        private void GameReset()
+        {
+            force = 12;
+            jumpSpeed = 0;
+            jumping = false;
+            score = 0;
+            obstacleSpeed = 10;
+            txtScore.Text = "Score: " + score;
+            TRex.Image = Properties.Resources.running; //changes to running image
+            isGameOver = false;
+            TRex.Top = 301;
+
+            foreach (Control x in this.Controls) //looks through all components
+            {
+                if (x is PictureBox && (string)x.Tag == "obstacle") //if component is PictureBox, ect
+                {
+                    //finds width of form, adds rand # in range to 1 of the obstacles, *10 so its far away when game starts; saved to position 
+                    position = this.ClientSize.Width + rand.Next(500, 800) + (x.Width * 10);
+
+                    x.Left = position;
+                }
+            }
+
+            gameTimer.Start();
 
         }
     }
